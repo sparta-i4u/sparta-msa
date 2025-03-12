88e075fe94452806265e0be5d09de710238f9598
ssu <74248726+azuressu@users.noreply.github.com>
2025-03-12 18:59:15 +0900

    [MSA-36] Eureka Server 에너테이션 추가

--- eureka-server/src/main/java/com/sprata/i4u/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/sprata/i4u/eureka_server/EurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.sprata.i4u.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
