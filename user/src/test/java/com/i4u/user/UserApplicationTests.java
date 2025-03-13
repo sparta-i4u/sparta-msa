cf524e54046c9f075fdb2bfe748aa8b3d20cfa98
dbp-jack <dbp100402@gmail.com>
2025-03-13 17:52:43 +0900

    feat: initial commit

---
+++ user/src/test/java/com/i4u/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i4u.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
