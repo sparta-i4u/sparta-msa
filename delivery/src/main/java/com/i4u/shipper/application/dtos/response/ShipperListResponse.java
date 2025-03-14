957a771b362c19e916c55318eceeee2e3662c694
azuressu <martina0130@naver.com>
2025-03-14 17:40:40 +0900

    [MSA-46] Shipper Dtos 수정

---
+++ delivery/src/main/java/com/i4u/shipper/application/dtos/response/ShipperListResponse.java
@@ -0,0 +1,41 @@
+package com.i4u.shipper.application.dtos.response;
+
+import java.util.UUID;
+
+import com.i4u.shipper.domain.entity.Shipper;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ShipperListResponse {
+
+	// 배송 담당자 ID
+	public UUID shipperId;
+
+	// 배송 담당자가 속한 허브 ID
+	public UUID hubId;
+
+	// 배송 담당자 타입 (허브/업체)
+	public String shipperType;
+
+	// 배송 담당자의 배송 순번
+	public Integer shipperOrder;
+
+	// 배송 담당자의 사용자 ID
+	public UUID userId;
+
+	public static ShipperListResponse toDto(Shipper shipper) {
+		return ShipperListResponse.builder()
+			.shipperId(shipper.getShipperId())
+			.hubId(shipper.getHubId())
+			.shipperType(shipper.getShipperType().toString())
+			.shipperOrder(shipper.getShipperOrder())
+			.userId(shipper.getUserId())
+			.build();
+	}
+
+}
