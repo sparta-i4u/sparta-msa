4e9e6306e4f4a244fb46a66d42b78510e4b2fa5b
azuressu <martina0130@naver.com>
2025-03-14 17:44:42 +0900

    [MSA-53] Shipper Exception 생성

---
+++ delivery/src/main/java/com/i4u/shipper/presentation/exception/ShipperException.java
@@ -0,0 +1,12 @@
+package com.i4u.shipper.presentation.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.i4u.common.exception.CustomException;
+
+public class ShipperException extends CustomException {
+	public ShipperException(String message, HttpStatus status) {
+		// Shipper ErrorCode는 600번대로 고정
+		super("400", message, status);
+	}
+}
