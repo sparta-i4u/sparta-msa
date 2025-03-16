30532a9b18356a36aa8188e0d6e885bc8ec9e1cf
ahyun <kaeruruk@naver.com>
2025-03-16 22:05:18 +0900

    product 브랜치 테스트

--- product/src/main/java/com/i4u/product/ProductApplication.java
+++ product/src/main/java/com/i4u/product/ProductApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductApplication {
 
+	//product branch test
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
 	}
