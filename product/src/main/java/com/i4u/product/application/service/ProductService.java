2bb209641b798c88e4bb2d88b286c1bdad804e40
ahyun <kaeruruk@naver.com>
2025-03-17 23:34:35 +0900

    [MSA-79] ProductService 수정 - 상품 전체 조회 변경

--- product/src/main/java/com/i4u/product/application/service/ProductService.java
+++ product/src/main/java/com/i4u/product/application/service/ProductService.java
@@ -29,8 +29,8 @@ public class ProductService {
     private final CompanyRepository companyRepository;
 
     //상품 생성
+    @Transactional
     public ProductResponse createProduct(final ProductCreateRequest request){
-
         //허브아이디와 컴퍼니 아이디 받아오기
         final UUID hubId = request.hubId();
         final UUID companyId = request.companyId();
@@ -44,41 +44,41 @@ public class ProductService {
         if (!companyRepository.existsById(companyId)) {
             throw new IllegalArgumentException("업체가 존재하지 않습니다: " + companyId);
         }
-
         final Product product = new Product(hubId, companyId, request.name(), request.price(), request.content());
         Product saved = productRepository.save(product);
         return ProductResponse.of(saved);
     }
 
-    //상품 목록 조회 - 모든 상품 전체 조회
+    //상품 목록 조회 - 필터링
     //모든 조회 및 검색에서 deleted_at 필드가 null인 데이터만을 대상으로 처리
-    public ProductSearchResponse search(final ProductSearchCond cond, final int page,
-                                        final int size, final String sort) {
+    public ProductSearchResponse search(final ProductSearchCond cond, final int page, final int size, final String sort) {
         Pageable pageable = getPageable(page, size, sort);
         return ProductSearchResponse.of(productRepository.search(cond, pageable));
     }
 
+    //모든 상품 전체 조회
+    //모든 조회 및 검색에서 deleted_at 필드가 null인 데이터만을 대상으로 처리
+    public ProductSearchResponse findAll(final int page, final int size, final String sort) {
+        Pageable pageable = getPageable(page, size, sort);
+        return ProductSearchResponse.of(productRepository.findAll(pageable));
+    }
+
     //페이징 함수
     private Pageable getPageable(final int page, final int size, final String sort) {
-
         String[] sortParams = sort.split(",");
         List<Sort.Order> orders = new ArrayList<>();
-
         for (String param : sortParams) {
             String[] fieldAndDirection = param.trim().split("-");
             if (fieldAndDirection.length != 2) {
                 throw new IllegalArgumentException(
                         "Invalid sort parameter format. Expected 'field direction'.");
             }
-
             String field = fieldAndDirection[0];
             String direction = fieldAndDirection[1].toUpperCase();
             Sort.Direction dir = direction.equals("ASC") ? Sort.Direction.ASC : Sort.Direction.DESC;
             orders.add(new Sort.Order(dir, field));
         }
-
         Sort sortObj = Sort.by(orders);
-
         return PageRequest.of(page, size, sortObj);
     }
 
@@ -99,9 +99,12 @@ public class ProductService {
     //상품 삭제
     //상품 엔티티의 deleted_at, deleted_by 필드를 이용하여 논리적 삭제를 관리합니다.
     // 상품이 삭제될 때 연관된 데이터(주문 등)도 삭제 관련 필드를 통해 관리합니다.
+    @Transactional
     public void softDeleteProducts(final List<UUID> productIds){
         productIds.stream().map(this::findProductById).forEach(Product::softDelete);
-    } //스트림으로 변환해 데이터 처리
+    }
+
+    //스트림으로 변환해 데이터 처리
     //findProductById 호출해 UUID-> Product로 변환
     //.forEach 는 반복문 같은 역할, 하나씩 뽑아서
     // product1.softDelete();
