4ec2134885c1cf0ed633ce1b9eb7ef4c30872019
azuressu <martina0130@naver.com>
2025-03-18 16:39:31 +0900

    [MSA-43] feat : Delivery Dto 생성

---
+++ delivery/src/main/java/com/i4u/delivery/application/dtos/request/DeliveryUpdateRequest.java
@@ -0,0 +1,36 @@
+package com.i4u.delivery.application.dtos.request;
+
+import java.util.UUID;
+
+import com.i4u.delivery.domain.entity.Delivery;
+import com.i4u.delivery.domain.entity.DeliveryState;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class DeliveryUpdateRequest {
+
+	// 배송지 주소
+	private String address;
+
+	// 수령인 ID
+	private UUID recipientId;
+
+	// 수령인 Slack ID
+	private String recipientSlackId;
+
+	public Delivery toDelivery(UUID recipientHubId, UUID shipperId, String userSlackId) {
+		return Delivery.builder()
+			.arriveHubId(recipientHubId)
+			.shipperId(shipperId)
+			.address(this.address)
+			.recipientId(this.recipientId)
+			.recipientSlackId(userSlackId)
+			.build();
+	}
+
+}
