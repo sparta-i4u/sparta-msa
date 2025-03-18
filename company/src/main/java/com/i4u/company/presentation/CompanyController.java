b48234ec1c228df48fc7b6187b424992bcb1dcb3
ahyun <kaeruruk@naver.com>
2025-03-18 12:44:25 +0900

    [MSA-98] CompanyController 생성

---
+++ company/src/main/java/com/i4u/company/presentation/CompanyController.java
@@ -0,0 +1,65 @@
+package com.i4u.company.presentation;
+
+
+import com.i4u.common.utils.CommonResponse;
+import com.i4u.company.application.dto.request.CompanyRequestDto;
+import com.i4u.company.application.dto.response.CompanyResponseDto;
+import com.i4u.company.application.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/companies")
+@RestController
+@RequiredArgsConstructor
+public class CompanyController {
+
+    private final CompanyService companyService;
+
+    //업체 생성
+    //@Secured
+    @PostMapping("")
+    public ResponseEntity<CommonResponse> createCompany(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            final @RequestBody CompanyRequestDto requestDto) {
+        CompanyResponseDto response = companyService.createCompany(userDetails,requestDto);
+        return new ResponseEntity<>(ResponseVOUtils.getSuccessResponse(response), HttpStatus.CREATED);
+    }
+
+    //업체 조회
+    //@Secured
+    @GetMapping("")
+    public ResponseEntity<CommonResponse> getCompany(
+            @RequestParam String keyword,
+            @RequestParam int page,
+            @RequestParam int size,
+            @RequestParam String sortBy,
+            @RequestParam boolean isAsc) {
+        Page<CompanyResponseDto> response = companyService.getCompany(keyword, page, size, sortBy, isAsc);
+        return new ResponseEntity<>(ResponseVOUtils.getSuccessResponse(response), HttpStatus.OK);
+    }
+
+    //업체 수정
+    //@Secured({UserRoleEnum.Authority.MANAGER, UserRoleEnum.Authority.MASTER, UserRoleEnum.Authority.OWNER})
+    @PutMapping("/{companyId}")
+    public ResponseEntity<CommonResponse> updateCompany(
+            @PathVariable UUID companyId, final @RequestBody CompanyRequestDto requestDto) {
+        CompanyResponseDto response = companyService.updateCompany(companyId, requestDto);
+        return new ResponseEntity<>(ResponseVOUtils.getSuccessResponse(response), HttpStatus.OK);
+    }
+
+    //업체 삭제
+   // @Secured({UserRoleEnum.Authority.MANAGER, UserRoleEnum.Authority.MASTER, UserRoleEnum.Authority.OWNER})
+    @DeleteMapping("/{companyId}")
+    public ResponseEntity<CommonResponse> deleteCompany(@PathVariable UUID companyId) {
+        CompanyResponseDto response = companyService.deleteCompany(companyId);
+        return new ResponseEntity<>(ResponseVOUtils.getSuccessResponse(response), HttpStatus.OK);
+    }
+
+}
