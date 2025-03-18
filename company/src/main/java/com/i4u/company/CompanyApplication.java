6241ecde801d59479d87de6a3d054ead2f657c5c
ahyun <kaeruruk@naver.com>
2025-03-18 10:41:43 +0900

    [MSA-97] Company 브랜치생성

--- company/src/main/java/com/i4u/company/CompanyApplication.java
+++ company/src/main/java/com/i4u/company/CompanyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CompanyApplication {
 
+	//Company branch 생성
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyApplication.class, args);
 	}
