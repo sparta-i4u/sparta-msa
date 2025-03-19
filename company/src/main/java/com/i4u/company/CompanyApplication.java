e26ecbf17fb461e87987782d797c5c20444e7421
ahyun <kaeruruk@naver.com>
2025-03-20 01:50:03 +0900

    [MSA-112] refactor: Company 패키지 Q객체 인식 불가 해결 - build로 해결

--- company/src/main/java/com/i4u/company/CompanyApplication.java
+++ company/src/main/java/com/i4u/company/CompanyApplication.java
@@ -2,17 +2,16 @@ package com.i4u.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = {
-		SecurityAutoConfiguration.class,
-		ManagementWebSecurityAutoConfiguration.class
-})
+@SpringBootApplication
+//		(exclude = {
+//		SecurityAutoConfiguration.class
+//		ManagementWebSecurityAutoConfiguration.class
+//})
 @EnableFeignClients
-@EnableJpaAuditing
+@ComponentScan(basePackages = {"com.i4u.common", "com.i4u.company"})
 public class CompanyApplication {
 
 	//Company branch 생성
