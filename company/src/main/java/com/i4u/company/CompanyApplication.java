83d6733cb63491b0c351aec1a8dcacd8f3da65e9
ahyun <kaeruruk@naver.com>
2025-03-19 15:33:28 +0900

    Company 서버 셋팅

--- company/src/main/java/com/i4u/company/CompanyApplication.java
+++ company/src/main/java/com/i4u/company/CompanyApplication.java
@@ -2,8 +2,17 @@ package com.i4u.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		ManagementWebSecurityAutoConfiguration.class
+})
+@EnableFeignClients
+@EnableJpaAuditing
 public class CompanyApplication {
 
 	//Company branch 생성
