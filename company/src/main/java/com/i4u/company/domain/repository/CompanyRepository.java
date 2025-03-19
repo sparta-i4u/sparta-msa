6663a71583bc196dfc43d67b242986c817037f6e
ahyun <kaeruruk@naver.com>
2025-03-20 01:55:09 +0900

    [MSA-100] refactor: Company Repository 파일들 수정

--- company/src/main/java/com/i4u/company/domain/repository/CompanyRepository.java
+++ company/src/main/java/com/i4u/company/domain/repository/CompanyRepository.java
@@ -1,12 +1,9 @@
 package com.i4u.company.domain.repository;
 
-import com.i4u.company.domain.Company;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.i4u.company.domain.entity.Company;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
--- company/src/main/java/com/i4u/company/infrastructure/CompanyRepositoryImpl.java
+++ company/src/main/java/com/i4u/company/infrastructure/CompanyRepositoryImpl.java
@@ -1,19 +1,7 @@
 package com.i4u.company.infrastructure;
 
 
-import com.i4u.company.domain.Company;
-import com.i4u.company.domain.repository.CompanyJpaRepository;
 import com.i4u.company.domain.repository.CompanyRepository;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 public abstract class CompanyRepositoryImpl implements CompanyRepository {
 
