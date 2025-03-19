5696fd43bb0c4c935819d4ed0703e3b78f2984dd
sall6550 <keinpark1@naver.com>
2025-03-19 10:38:49 +0900

    [MSA-94] feat: HubConnection 엔티티에 @SQLRestriction 어노테이션 추가

--- hub/src/main/java/com/i4u/hub/domain/model/HubConnection.java
+++ hub/src/main/java/com/i4u/hub/domain/model/HubConnection.java
@@ -4,6 +4,7 @@ import com.i4u.common.entity.Basic;
 import com.i4u.hub.application.dtos.hubConnection.UpdateHubConnectionReqDto;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.util.UUID;
 
@@ -12,6 +13,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@SQLRestriction("is_deleted IS NULL OR is_deleted = false")
 @Table(name = "p_hub_connection")
 public class HubConnection extends Basic {
 
