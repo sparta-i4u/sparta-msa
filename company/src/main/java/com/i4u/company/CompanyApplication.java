722d8ea33497c2e3bd0cb4b09d9938047c985a69
ahyun <kaeruruk@naver.com>
2025-03-21 00:19:15 +0900

    new Company branch 생성

--- company/src/main/java/com/i4u/company/CompanyApplication.java
+++ company/src/main/java/com/i4u/company/CompanyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CompanyApplication {
 
+	//new Companybranch 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyApplication.class, args);
 	}
