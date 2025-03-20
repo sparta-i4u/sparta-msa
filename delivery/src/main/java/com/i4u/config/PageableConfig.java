bf8a2946983356e147f9ab63c79a0222b7f8feaf
azuressu <martina0130@naver.com>
2025-03-20 21:42:01 +0900

    [MSA-91] fix : 실행 오류 수정

--- delivery/src/main/java/com/i4u/config/PageableConfig.java
+++ delivery/src/main/java/com/i4u/config/PageableConfig.java
@@ -1,4 +1,4 @@
-package com.i4u.delivery.infrastructure.config;
+package com.i4u.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
