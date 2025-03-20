0f4105cc893c7241ace87a6f19bf3a907436c22b
ahyun <kaeruruk@naver.com>
2025-03-20 23:59:52 +0900

    Revert "refactor: Company 실행 안되는 오류 해결 - 정상 실행(yml파일 위치 설정 오류)"
    This reverts commit e047dfede4633ee97ace64f61260b8ccc072d462.

--- company/src/main/java/com/i4u/company/presentation/controller/CompanyController.java
+++ company/src/main/java/com/i4u/company/presentation/controller/CompanyController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
