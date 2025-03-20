c894f1dcda4ec8f285aaaf1c3525991491799fdb
azuressu <martina0130@naver.com>
2025-03-20 21:42:01 +0900

    [MSA-91] fix : 실행 오류 수정

--- delivery/src/main/java/com/i4u/client/HubUserClient.java
+++ delivery/src/main/java/com/i4u/client/HubUserClient.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import reactor.core.publisher.Mono;
 
-@FeignClient(path = "/api/v1/gateway")
-public interface HubUserClient {
-
-	@GetMapping("/shipper-info/{userId}/{hubId}")
-	Mono<Map<String, Object>> getShipperInfo(@PathVariable UUID userId, @PathVariable UUID hubId);
-
-	@GetMapping("/shipper-info/{hubId}")
-	Mono<Map<String, Object>> getShipperInfoFromHub(@PathVariable UUID hubId);
-
-}
+// @FeignClient(path = "/api/v1/gateway")
+// public interface HubUserClient {
+//
+// 	@GetMapping("/shipper-info/{userId}/{hubId}")
+// 	Mono<Map<String, Object>> getShipperInfo(@PathVariable UUID userId, @PathVariable UUID hubId);
+//
+// 	@GetMapping("/shipper-info/{hubId}")
+// 	Mono<Map<String, Object>> getShipperInfoFromHub(@PathVariable UUID hubId);
+//
+// }
