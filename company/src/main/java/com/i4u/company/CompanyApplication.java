e047dfede4633ee97ace64f61260b8ccc072d462
ahyun <kaeruruk@naver.com>
2025-03-20 23:42:36 +0900

    refactor: Company 실행 안되는 오류 해결 - 정상 실행(yml파일 위치 설정 오류)

--- company/src/main/java/com/i4u/company/CompanyApplication.java
+++ company/src/main/java/com/i4u/company/CompanyApplication.java
@@ -1,15 +1,16 @@
 package com.i4u.company;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-//		(exclude = {
-//		SecurityAutoConfiguration.class
-//		ManagementWebSecurityAutoConfiguration.class
-//})
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		ManagementWebSecurityAutoConfiguration.class
+})
 @EnableFeignClients
 @ComponentScan(basePackages = {"com.i4u.common", "com.i4u.company"})
 public class CompanyApplication {
