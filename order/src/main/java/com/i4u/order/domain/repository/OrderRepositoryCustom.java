8bc59daf1cc77fc62997c9af2a4e35ceab8cf1b8
ssu <74248726+azuressu@users.noreply.github.com>
2025-03-21 01:28:00 +0900

    [MSA-91] fix : 충돌 오류 해결

--- order/src/main/java/com/i4u/order/domain/repository/OrderRepositoryCustom.java
+++ order/src/main/java/com/i4u/order/domain/repository/OrderRepositoryCustom.java
@@ -6,6 +6,13 @@ import org.springframework.data.web.PagedModel;
 import com.i4u.order.application.dtos.request.OrderSearchRequest;
 import com.i4u.order.application.dtos.response.OrderGetListResponse;
 
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.web.PagedModel;
+
+import com.i4u.order.application.dtos.request.OrderSearchRequest;
+import com.i4u.order.application.dtos.response.OrderGetListResponse;
+
 public interface OrderRepositoryCustom {
 
 	PagedModel<OrderGetListResponse> searchOrder(Pageable pageable, OrderSearchRequest request);
