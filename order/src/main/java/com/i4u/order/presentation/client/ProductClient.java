847a9bbc47acf6279fae6d4b8aeadbd26b382639
ssu <74248726+azuressu@users.noreply.github.com>
2025-03-21 01:36:55 +0900

    [MSA-91] fix : 충돌 오류 해결

--- order/src/main/java/com/i4u/order/presentation/client/ProductClient.java
+++ order/src/main/java/com/i4u/order/presentation/client/ProductClient.java
@@ -1,9 +1,14 @@
 package com.i4u.order.presentation.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.i4u.common.utils.CommonResponse;
 import com.i4u.order.presentation.dtos.request.OrderProductRequest;
 import com.i4u.order.presentation.dtos.request.OrderProductStateUpdateRequest;
 import com.i4u.order.presentation.dtos.request.OrderProductUpdateRequest;
@@ -13,12 +18,13 @@ import com.i4u.order.presentation.dtos.response.OrderProductUpdateResponse;
 @FeignClient(name = "product")
 public interface ProductClient {
 
-	@GetMapping("/products/{productId}")
-	OrderProductResponse confirmProduct(@ModelAttribute OrderProductRequest request /*userId, userRole or JWT 필요*/);
+	@GetMapping("/api/v1/products/order")
+	ResponseEntity<CommonResponse<OrderProductResponse>> confirmProduct(@ModelAttribute OrderProductRequest request /*userId, userRole or JWT 필요*/);
 
-	@GetMapping("/products/{productId}/new-info")
-	OrderProductUpdateResponse confirmProductUpdate(OrderProductUpdateRequest build);
+	@PatchMapping("/api/v1/products/{productId}/update-product")
+	ResponseEntity<CommonResponse<OrderProductUpdateResponse>> confirmProductUpdate(@RequestBody OrderProductUpdateRequest request);
 
-	@GetMapping("/products/{productId}/update-product")
-	void updateProductState(OrderProductStateUpdateRequest build);
+	@PatchMapping("/api/v1/products/{productId}/update-product")
+	ResponseEntity<CommonResponse> updateProductState(@RequestBody OrderProductStateUpdateRequest request);
+
 }
