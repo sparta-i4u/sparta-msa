e1c2ae4c765f4586151057a353ec5ed85e9bda4f
ahyun <kaeruruk@naver.com>
2025-03-20 11:42:09 +0900

    [MSA-80] Product DTO 수정 - 상품개수추가

--- product/src/main/java/com/i4u/product/application/dto/request/ProductCreateRequest.java
+++ product/src/main/java/com/i4u/product/application/dto/request/ProductCreateRequest.java
@@ -10,5 +10,7 @@ public record ProductCreateRequest(
         @NotNull(message = "업체아이디는 필수입니다") UUID companyId,
         @NotBlank(message = "상품 이름은 필수입니다.") String name,
         @NotNull(message = "상품 가격은 필수입니다.") Integer price,
-        @NotBlank(message = "상품 설명은 필수입니다.") String content) {
+        @NotBlank(message = "상품 설명은 필수입니다.") String content,
+        @NotNull(message = "상품 개수는 필수입니다.") Integer count
+) {
 }
