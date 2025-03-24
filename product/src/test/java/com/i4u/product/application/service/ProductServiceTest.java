2548897985596ab7aa5d3d9e0974fc9f7299f8b4
ahyun <kaeruruk@naver.com>
2025-03-24 15:18:56 +0900

    [MSA-145] test: Product Test코드 수정

--- product/src/test/java/com/i4u/product/application/service/ProductServiceTest.java
+++ product/src/test/java/com/i4u/product/application/service/ProductServiceTest.java
@@ -1,151 +1,140 @@
-//package com.i4u.product.application.service;
-//
-//import com.i4u.order.presentation.client.CompanyClient;
-//import com.i4u.order.presentation.client.HubClient;
-//import com.i4u.product.application.dto.request.ProductCreateRequest;
-//import com.i4u.product.application.dto.request.ProductUpdateRequest;
-//import com.i4u.product.application.dto.response.ProductResponse;
-//import com.i4u.product.application.dto.response.ProductSearchResponse;
-//import com.i4u.product.domain.Product;
-//import com.i4u.product.domain.repository.ProductQueryRepository;
-//import com.i4u.product.domain.repository.ProductRepository;
-//
-//import com.i4u.product.exception.ProductNotFoundException;
-//import org.junit.jupiter.api.*;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.PageImpl;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import static com.i4u.product.domain.QProduct.product;
-//import static org.assertj.core.api.Assertions.*;
-//import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
-//import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.Mockito.*;
-//
-//import java.util.Collections;
-//import java.util.List;
-//import java.util.Optional;
-//import java.util.UUID;
-//
-//import static org.mockito.Mockito.when;
-//import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
-//
-//
-//// 테스트 : 상품 등록 성공
-//// 테스트 : 존재하지 않는 상품 ID 조회 시 예외 발생
-//// 테스트 : 전체 상품 조회(페이지네이션) 성공
-//// 테스트 : 상품 수정 성공
-//// 테스트 : 상품 삭제 성공
-//// 테스트 : 상품 삭제 실패
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
-//class ProductServiceTest {
-//
-//    @Mock
-//    private ProductRepository productRepository;
-//
-//    @Mock
-//    private ProductQueryRepository productQueryRepository;
-//
-//    @Mock
-//    private HubClient hubClient;
-//
-//    @Mock
-//    private CompanyClient companyClient;
-//
-//    @InjectMocks  //목으로 생성된 레파지토리 주입
-//    private ProductService productService;
-//
-//    private UUID productId;
-//    private UUID companyId;
-//    private UUID hubId;  // ✅ hubId 선언 추가
-//
-////    public Product(final UUID hubId, final UUID companyId, final String name, final Integer price , final String content, final Integer count) {
-////        this.hubId = hubId;
-////        this.companyId = companyId;
-////        this.name =  name;
-////        this.content = content;
-////        this.price = price;
-////        this.count = count;
-////        this.isDeleted = false;
-////    }
-//
-//    @BeforeEach
-//    void setUp() {  //각각의 테스트코드가 실행되기 전 수행되는 메소드
-//        productId = UUID.randomUUID(); // UUID 생성
-//        companyId = UUID.randomUUID();
-//        hubId = UUID.randomUUID();
-//        Product product = new Product(hubId, companyId, "상품", 10000, "상품설명",10); // 상품 객체 생성
-//        ProductUpdateRequest request = new ProductUpdateRequest("새 상품명", "새 설명", 1000, 10);
+package com.i4u.product.application.service;
+
+import com.i4u.product.presentation.client.CompanyClient;
+import com.i4u.product.presentation.client.HubClient;
+import com.i4u.product.application.dto.request.ProductCreateRequest;
+import com.i4u.product.application.dto.request.ProductUpdateRequest;
+import com.i4u.product.application.dto.response.ProductResponse;
+
+import com.i4u.product.domain.Product;
+import com.i4u.product.domain.repository.ProductQueryRepository;
+import com.i4u.product.domain.repository.ProductRepository;
+
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import static org.mockito.Mockito.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+
+
+
+// 테스트 : 상품 등록 성공
+// 테스트 : 존재하지 않는 상품 ID 조회 시 예외 발생
+// 테스트 : 전체 상품 조회(페이지네이션) 성공
+// 테스트 : 상품 수정 성공
+// 테스트 : 상품 삭제 성공
+// 테스트 : 상품 삭제 실패
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private ProductQueryRepository productQueryRepository;
+
+    @Mock
+    private HubClient hubClient;
+
+    @Mock
+    private CompanyClient companyClient;
+
+    @InjectMocks
+    private ProductService productService;  //mock으로 두면 service 안돌아가 ㅠㅠ
+
+    private UUID productId;
+    private UUID companyId;
+    private UUID hubId;  // ✅ hubId 선언 추가
+
+//    public Product(final UUID hubId, final UUID companyId, final String name, final Integer price , final String content, final Integer count) {
+//        this.hubId = hubId;
+//        this.companyId = companyId;
+//        this.name =  name;
+//        this.content = content;
+//        this.price = price;
+//        this.count = count;
+//        this.isDeleted = false;
 //    }
-//
-//    @AfterEach
-//    void tearDown() {  //각각의 테스트 코드가 실행된 후 실행
-//    }
-//
-//    @BeforeAll //모든 테스트 코드가 실행되기 전 최초로 실행
-//    static void beforeAll(){
-//    }
-//
-//    @AfterAll  //모든 테스트코드가 수행된 후 마지막으로 수행
-//    static void afterAll(){}
-//
-//
-//    @Test
-//    @DisplayName("상품 생성 성공 테스트코드") //테스트 내용 한눈에 알아보게 네이밍
-//    void testCreateProduct_success() {
-//        // given: 상품 등록 요청 데이터 생성
-//        UUID hubId = UUID.randomUUID();
-//        UUID companyId= UUID.randomUUID();
-//        UUID userId = UUID.randomUUID();
-//        String role = "MASTER";
-//        ProductCreateRequest requestDto = new ProductCreateRequest(
-//                hubId, // hubId
-//                companyId, // companyId
-//                "테스트 상품", // 상품명
-//                1000, // 가격
-//                "테스트 상품 설명", // 설명
-//                10 // 수량
-//        );
-//
-//        // HubClient mock 설정
-//        when(hubClient.getHubIdFromOrder(any(UUID.class))).thenReturn(requestDto.hubId());  // mock HubClient
-//
-//        // CompanyClient mock 설정
-//        when(companyClient.getCompanyId(any(UUID.class))).thenReturn(companyId);
-//
-//
-//        // ProductService에서 반환할 상품 객체 생성
-//        Product mockProduct = new Product(requestDto.hubId(), requestDto.companyId(), requestDto.name(),
-//                requestDto.price(), requestDto.content(), requestDto.count());
-//        mockProduct.setId(UUID.randomUUID());  // 생성된 상품의 ID 설정
-//
-//        // ProductRepository가 반환할 가짜 저장된 상품 객체
-//        when(productRepository.save(any(Product.class))).thenReturn(mockProduct);
-//
-//        // when: createProduct 메서드 호출
-//        ProductResponse response = productService.createProduct(requestDto, userId.toString(), role);
-//
-//        // then: 반환된 DTO 값 검증
-//        assertThat(response).isNotNull();
-//        assertThat(response.getId()).isNotNull();
-//        assertThat(response.getName()).isEqualTo("테스트 상품");
-//        assertThat(response.getPrice()).isEqualTo(1000);
-//        assertThat(response.getContent()).isEqualTo("테스트 상품 설명");
-//
-//        // productRepository가 1번 호출되었는지 확인
-//        verify(productRepository, times(1)).save(any(Product.class));
-//    }
-//
+
+    @BeforeEach
+    void setUp() {  //각각의 테스트코드가 실행되기 전 수행되는 메소드
+        productId = UUID.randomUUID(); // UUID 생성
+        companyId = UUID.randomUUID();
+        hubId = UUID.randomUUID();
+        Product product = new Product(hubId, companyId, "상품", 10000, "상품설명",10); // 상품 객체 생성
+        ProductUpdateRequest request = new ProductUpdateRequest("새 상품명", "새 설명", 1000, 10);
+    }
+
+    @AfterEach
+    void tearDown() {  //각각의 테스트 코드가 실행된 후 실행
+    }
+
+    @BeforeAll //모든 테스트 코드가 실행되기 전 최초로 실행
+    static void beforeAll(){
+    }
+
+    @AfterAll  //모든 테스트코드가 수행된 후 마지막으로 수행
+    static void afterAll(){}
+
+
+    @Test
+    @DisplayName("상품 생성 성공 테스트코드") //테스트 내용 한눈에 알아보게 네이밍
+    void testCreateProduct_success() {
+        // given: 상품 등록 요청 데이터 생성
+        UUID hubId = UUID.randomUUID();
+        UUID companyId= UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+        String role = "MASTER";
+        ProductCreateRequest requestDto = new ProductCreateRequest(
+                hubId, // hubId
+                companyId, // companyId
+                "테스트 상품", // 상품명
+                1000, // 가격
+                "테스트 상품 설명", // 설명
+                10 // 수량
+        );
+
+        when(hubClient.getHubId(any(UUID.class))).thenReturn(requestDto.hubId());  // mock HubClient
+
+        // CompanyClient mock 설정
+        when(companyClient.getCompanyId(any(UUID.class))).thenReturn(companyId);        // HubClient mock 설정
+
+
+        // ProductService에서 반환할 상품 객체 생성
+        Product mockProduct = new Product(requestDto.hubId(), requestDto.companyId(), requestDto.name(),
+                requestDto.price(), requestDto.content(), requestDto.count());
+
+        mockProduct.setId(UUID.randomUUID());  // 생성된 상품의 ID 설정
+
+        // ProductRepository가 반환할 가짜 저장된 상품 객체
+        when(productRepository.save(any(Product.class))).thenReturn(mockProduct);
+
+        // when: createProduct 메서드 호출
+        ProductResponse response = productService.createProduct(requestDto, userId.toString(), role);
+
+        // then: 반환된 DTO 값 검증
+        assertThat(response).isNotNull();
+        assertThat(response.getId()).isNotNull();
+        assertThat(response.getName()).isEqualTo("테스트 상품");
+        assertThat(response.getPrice()).isEqualTo(1000);
+        assertThat(response.getContent()).isEqualTo("테스트 상품 설명");
+
+        // productRepository가 1번 호출되었는지 확인
+        verify(productRepository, times(1)).save(any(Product.class));
+    }
+
 //    //상품 전체 조회
 //    @Test
 //    public void testFindAll() {
@@ -295,5 +284,5 @@
 //        assertThrows(ProductNotFoundException.class, () ->
 //                productService.softDeleteProducts(productIds, userId, role));
 //    }
-//}
-//
+}
+
